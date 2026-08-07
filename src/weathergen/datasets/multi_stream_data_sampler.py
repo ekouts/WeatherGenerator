5ca12f9d5f714c2befda8a1e9ded16f11fab6ff5
Florian Scheidl <f.scheidl@fz-juelich.de>
2026-08-07 10:40:32 +0200

    Spatial shard interface

--- src/weathergen/datasets/multi_stream_data_sampler.py
+++ src/weathergen/datasets/multi_stream_data_sampler.py
@@ -35,6 +35,7 @@ from weathergen.datasets.utils import (
 from weathergen.readers_extra.registry import get_extra_reader
 from weathergen.train.utils import Stage, get_batch_size_from_config
 from weathergen.utils.distributed import get_encoder_spatial_parallel_size, is_root
+from weathergen.utils.spatial_shard import SpatialShard
 
 type AnyDataReader = DataReaderBase | DataReaderAnemoi | DataReaderObs
 type StreamName = str
@@ -103,23 +104,23 @@ class MultiStreamDataSampler(torch.utils.data.IterableDataset):
         # Ranks in one encoder-spatial group must consume the same batch. Data
         # parallelism therefore operates across groups, not across individual ranks.
         spatial_parallel_size = get_encoder_spatial_parallel_size(cf)
-        self.spatial_parallel_size = spatial_parallel_size
-        self.spatial_parallel_rank = cf.rank % spatial_parallel_size
+        self.spatial_shard = SpatialShard.from_global_rank(
+            cf.healpix_level,
+            spatial_parallel_size,
+            cf.rank,
+        )
+        self.spatial_parallel_size = self.spatial_shard.spatial_parallel_size
+        self.spatial_parallel_rank = self.spatial_shard.spatial_rank
         self.rank = cf.rank // spatial_parallel_size
         self.world_size = cf.world_size // spatial_parallel_size
         self.repeat_data = cf.data_loading.get("repeat_data_in_mini_epoch", False)
 
         # initialise healpic
         self.healpix_level = cf.healpix_level
-        self.num_healpix_cells = 12 * 4**self.healpix_level
-        if self.num_healpix_cells % spatial_parallel_size:
-            raise ValueError(
-                f"number of HEALPix cells ({self.num_healpix_cells}) must be divisible by "
-                f"encoder_spatial_parallel_size ({spatial_parallel_size})"
-            )
-        self.local_num_healpix_cells = self.num_healpix_cells // spatial_parallel_size
-        self.local_cell_start = self.spatial_parallel_rank * self.local_num_healpix_cells
-        self.local_cell_end = self.local_cell_start + self.local_num_healpix_cells
+        self.num_healpix_cells = self.spatial_shard.num_cells
+        self.local_num_healpix_cells = self.spatial_shard.cells_per_rank
+        self.local_cell_start = self.spatial_shard.cell_start
+        self.local_cell_end = self.spatial_shard.cell_end
         self.masker = Masker(cf.healpix_level, stage, cf.streams, self.mode_cfg)
         self.tokenizer = TokenizerMasking(
             cf.healpix_level,
